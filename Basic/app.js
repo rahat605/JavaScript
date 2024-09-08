7da824f9e56844d7580fa00a18f5af4e8dcb5224
rahat605 <mdrahatrion2@gmail.com>
2024-09-08 15:57:27 +0600

    ifelse

--- Basic/app.js
+++ Basic/app.js
@@ -1,10 +1,21 @@
-let color = "red";
-if (color == "red"){
-    console.log("STOP!!");
-}
-if(color=="yellow"){
-    console.log("SLOW DOWN!!");
-}
-if(color=="green"){
-    console.log("GO!");
+// if else statement
+// let size = "XL";
+// if(size  == "XL"){
+//     console.log("The price is Rs.250.");
+// } else if(size == "L"){
+//     console.log("The price is Rs. 200.");
+// } else if (size == "M"){
+//     console.log("The price is Rs.100.")
+// } else if(size == "S"){
+//     console.log("The price is Rs.50.")
+// }
+
+let marks = 80;
+if(marks>33){
+    console.log("CONGRATES");
+    if(marks>=80){
+        console.log("YOU GOT A+");
+    }
+} else{
+    console.log("FAILED");
 }
