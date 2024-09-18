dca81bc9537d7d5b989a3299de76701096ddabc7
rahat605 <mdrahatrion2@gmail.com>
2024-09-19 00:56:27 +0600

    jsupdate

--- Trim/app.js
+++ Trim/app.js
@@ -3,3 +3,13 @@ let t= msg.trim();
 let s = msg.slice(0,5);
 console.log(t);
 console.log(s);
+let rep = msg.replace("h","a");
+console.log(rep);
+let rpt = msg.repeat(3);
+console.log(rpt);
+
+let msg2 = "help!";
+let out = msg2.trim().toUpperCase();
+console.log(out);
+
+  
---
+++ array/app.js
@@ -0,0 +1,12 @@
+let arr = ["Rahat",12,10,"Male"];
+let info = [2388,90];
+console.log(info);
+let fruits = ["Mango","Apple","Litchi"];
+console.log(fruits);
+console.log(fruits.push("Orange"));
+
+let months= ["Januray","July","March","August"];
+months.shift();
+months.shift();
+months.unshift("June");
+months.unshift("July");
