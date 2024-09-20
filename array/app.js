06d40676eced660782f38a896ab37021bf5d5094
rahat605 <mdrahatrion2@gmail.com>
2024-09-20 17:59:04 +0600

    js

--- array/app.js
+++ array/app.js
@@ -11,5 +11,33 @@ months.shift();
 months.unshift("June");
 months.unshift("July");
 
+// slice example
+
 let str = "Rahatg";
 console.log(str.slice(-2));
+
+// splice example
+
+let colors = ["red","yellow","orange","blue","green"]
+console.log(colors.splice(3));
+console.log(colors);
+console.log(colors.splice(0,0,"skyblue","black"));
+console.log(colors);
+console.log(colors.splice(3,0,"guu"));
+console.log(colors);
+
+// sorting
+console.log(colors.sort()) ;
+
+// practice question
+let mash = ["january","july","march","august"];
+mash.shift();
+mash.shift();
+mash.unshift("june");
+mash.unshift("july");
+console.log(mash);
+
+
+// task2
+let lan=["C","C++","C#","html"."javascriptg","pyhthon,"java","C#","SQL"];
+    lanrese
