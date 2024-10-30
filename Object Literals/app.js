b352ee7658db2535f5367a1ec82680f626b97687
rahat605 <mdrahatrion2@gmail.com>
2024-10-30 16:03:46 +0600

    ol

---
+++ Object Literals/app.js
@@ -0,0 +1,13 @@
+const student = {
+    name: "Rahat",
+    age: 25,
+    marks: 90
+};
+
+const post = {
+    username: "@rahat605",
+    content: "Winning Moment",
+    likes: 200,
+    reposts: 5,
+    tags: ["@borshon","@nahin","@rohan"]
+};
