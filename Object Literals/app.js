c0d8886e8409bb5f5e459c9a65d261e473022ef7
rahat605 <mdrahatrion2@gmail.com>
2024-11-01 23:07:32 +0600

    ol

--- Object Literals/app.js
+++ Object Literals/app.js
@@ -1,7 +1,8 @@
 const student = {
     name: "Rahat",
     age: 25,
-    marks: 90
+    marks: 90,
+    city: "Dhaka"
 };
 
 const post = {
@@ -11,3 +12,49 @@ const post = {
     reposts: 5,
     tags: ["@borshon","@nahin","@rohan"]
 };
+
+student.city = "Rangpur";
+student.result = 3.52;
+post.comments = 17;
+student.recent = ["SQA","Front End","Bekar"];
+
+//object of object
+
+const studentList = {
+    rahat:{
+        marks: 95,
+        grade: "A+",
+        city: "Dhaka"
+    },
+    rion:{
+        marks: 65,
+        grade: "C+",
+        city: "Rangpur"
+    },
+    maruf:{
+        marks: 80,
+        grade: "A+",
+        city: "Chittagoan"
+    }
+}
+
+// array of object
+
+const studentListArr = [
+    {   name:"Rahat",
+        marks: 95,
+        grade: "A+",
+        city: "Dhaka"},
+
+    {   name:"Rion",
+        marks: 65,
+        grade: "C+",
+        city: "Rangpur"},
+
+    {   name:"Maruf",
+        marks: 80,
+        grade: "A+",
+        city: "Chittagoan"}
+]
+
+// Math Object
