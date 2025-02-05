aff0e3500f2ba796abd239ef760902a5798ed903
rahat605 <mdrahatrion2@gmail.com>
2025-02-06 03:21:05 +0600

    task6

---
+++ Task6/app.js
@@ -0,0 +1,17 @@
+// Task 1
+let arr = [1,2,4,5,6,7,8,9,10];
+let number = 5;
+function testLarge(arr,number){
+    for(let i=0;i< arr.length;i++){
+        if(arr[i]>number){
+            console.log(arr[i]);
+        }
+    }
+}
+
+testLarge(arr,number);
+
+
+
+// Task 2
+let str = "abcdabcdefgggh";
