0f49aaab79d512e273f29c3fc3d5d6d784d18276
rahat605 <mdrahatrion2@gmail.com>
2025-02-19 06:10:59 +0600

    spread

--- every&some&reduce/app.js
+++ every&some&reduce/app.js
@@ -2,21 +2,30 @@ let testNum = [2, 4, 6]; //Example of every
 let ans = testNum.every((el) => el % 2 == 0);
 console.log(ans);
 
-let testSome = [2,4,6,5]; //Example of some
-let ans2 = testSome.some((el)=>el%2==0);
+let testSome = [2, 4, 6, 5]; //Example of some
+let ans2 = testSome.some((el) => el % 2 == 0);
 console.log(ans2);
 
-let reduceNum = [1,2,3,4]; //Example of reduce
-let finalVal = reduceNum.reduce((res,el)=>res+el);
+let reduceNum = [1, 2, 3, 4]; //Example of reduce
+let finalVal = reduceNum.reduce((res, el) => res + el);
 console.log(finalVal);
 
-let testArr = [1,2,3,4,13,6,7,8,9]; //Find the max value using reduce
-let maxVal = testArr.reduce((max,el)=> {
-    if(max<el){
-        return el;
-    }else{
-        return max;
-    }
-});
+let testArr = [1, 2, 3, 4, 13, 6, 7, 8, 9]; //Find the max value using reduce
+let maxVal = testArr.reduce((max, el) => {
+  if (max < el) {
+    return el;
+  } else {
+    return max;
+  }
+}); 
 console.log(maxVal);
-// Testing
+
+function sum(a, b = 2) {
+  //default parameter
+  return a + b;
+}
+console.log(sum(1));
+
+// Spread
+let arr = [1, 2, 3, 4, 5];
+console.log(Math.max(...arr));
