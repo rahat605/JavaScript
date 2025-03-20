8cb5d6d7ca52921154aa379a326bb0f33d8864a3
Rahat Ahmed <91186934+rahat605@users.noreply.github.com>
2025-03-21 03:49:53 +0600

    promise

--- functions/app.js
+++ functions/app.js
@@ -92,6 +92,36 @@ setInterval(() => {
   // console.log("Hi");
 }, 1000);
 
+// Async funtion 
+
+async function greet() {
+  throw "404 page not found! ";
+}
+
+greet()
+.then((result)=>{
+  console.log("Promise was resolved!");
+})
+.catch((err)=>{
+  console.log("Promise was rejected with err: ", err);
+});
+
+function getNum(){
+  return new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      let num = Math.floor(Math.random()*10)+1;
+      console.log(num);
+      resolve();
+    },1000);
+  })
+}
+
+async function demo() {
+  await getNum();
+  await getNum();
+}
+
+
 //Promise Resolve /reject
 function savetoData(data) {
   return new Promise((resolve, reject) => {
@@ -123,28 +153,54 @@ h1 = document.querySelector("h1");
 function changeColor(color,delay){
    return new Promise((resolve,reject)=>{
         setTimeout(()=>{
+          let num = Math.floor(Math.random()*5)+1;
+          if(num>3){
+            reject("Promise is rejected!");
+          }
             h1.style.color = color;
+            console.log(`Color changed to ${color}`);
             resolve("Color Changed!");
         },delay);
-    })
+    });
 }
 
-changeColor("red",1000)
-.then(()=>{
-    console.log("Color changed to RED");
-    return changeColor("orange",1000);
-})
-.then(()=>{
-    console.log("Color changed to Orange");
-    return changeColor("green",1000);
-})
-.then(()=>{
-    console.log("Color changed to Green");
-    return changeColor("blue",1000)
-})
-.then(()=>{
-    console.log("Color changed to Blue");
-})
+async function demo() {
+  try{
+    await changeColor("red",1000);
+    await changeColor("orange",1000);
+    await changeColor("green",1000);
+    await changeColor("blue",1000);
+  } catch(err){
+    console.log("Error caught!", err);
+  }
+
+  let a =5;
+  console.log(a);
+  console.log("new number =", a+3);
+  
+}
+
+demo();
+
+// changeColor("red",1000)
+// .then(()=>{
+//     console.log("Color changed to RED");
+//     return changeColor("orange",1000);
+// })
+// .then(()=>{
+//     console.log("Color changed to Orange");
+//     return changeColor("green",1000);
+// })
+// .then(()=>{
+//     console.log("Color changed to Green");
+//     return changeColor("blue",1000)
+// })
+// .then(()=>{
+//     console.log("Color changed to Blue");
+// })
+
+
+
 
 cube(3);
 sum(1, 2);
